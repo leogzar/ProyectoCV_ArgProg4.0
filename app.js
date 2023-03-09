97b91e0fdd3dfe59a6b30e695da05fd363781316
unknown <leogz.ar@gmail.ar>
2023-03-09 00:21:10 -0300

    ProyectoFinalCV_Arg.Prog.40

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const nombre = document.querySelector('#nombre');
+const correo = document.querySelector('#correo');
+const telefono = document.querySelector('#telefono');
+const foto = document.querySelector('#foto');
+
+const generarUsuario = async () => {
+    //consultar a la API
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+
+    foto.src = datos.picture.medium;
+    nombre.textContent = datos.name.first;
+    correo.textContent = datos.email;
+    telefono.textContent = datos.phone;
+}
+
+
+//crear un evento
+document.addEventListener('DOMContentLoaded', generarUsuario);
+boton.addEventListener('click', generarUsuario);
+
+
+
+
